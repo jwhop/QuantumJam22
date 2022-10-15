63dff95e879c40e7730055a37d69400f9201ec72
kahnivore <kahnivore@gmail.com>
2022-10-15 16:27:10 -0700

    Added splash scene, game scene, implemented start button, added font and TMP

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+
+    private Button _startButton;
+
+    private void Awake()
+    {
+        _startButton = GetComponent<Button>();
+
+    }
+    void Start()
+    {
+        
+         
+        if (_startButton != null)
+        {
+            _startButton.onClick.AddListener(() => ButtonClicked());
+
+        }
+    }
+
+    private void ButtonClicked()
+    {
+
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+    }
+
+}

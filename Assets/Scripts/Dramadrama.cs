b07c234939edf8390925e7e71bd072d6233effb3
kahnivore <kahnivore@gmail.com>
2022-10-20 15:04:05 -0700

    added penguin spawner

---
+++ Assets/Scripts/Dramadrama.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dramadrama : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetDramaMusic()
+    {
+
+    }
+
+   
+}
